d8f6161275b2504b15018ec16ab6b20ab3a04d34
Jose Vargas <rafael.uruguay2022@gmail.com>
2024-07-28 18:53:57 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('#nav')
+const abrir = document.querySelector('#abrir')
+const cerrar = document.querySelector('#cerrar')
+
+abrir.addEventListener("click", () =>{
+    nav.classList.add('visible')
+})
+
+cerrar.addEventListener("click", () =>{
+    nav.classList.remove('visible')
+})
+
+let btnsent = document.getElementById("btnFormulary")
+btnsent.addEventListener("click", saveFormulary)
+
+function saveFormulary (){
+    const formulary = document.getElementById("contactForm")
+    const dataFormulary = {
+        name: formulary.name.value,
+        email: formulary.email.value,
+        message: formulary.message.value
+    }
+
+    const dataJson = JSON.stringify (dataFormulary)
+
+    localStorage.setItem("formulary",dataJson)
+}
